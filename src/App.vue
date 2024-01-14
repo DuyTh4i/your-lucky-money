73d61ed159c8a0f097d15653d7748b0d6cd56108
DuyTh4i <duythai1315@gmail.com>
2024-01-14 15:42:19 +0700

    junk

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <TheModel></TheModel>
+  <!-- <TheFooter></TheFooter> -->
 </template>
 <script>
 import TheModel from "./components/TheModel.vue";
-
+import TheFooter from "./components/TheFooter.vue"
 export default {
   components: {
     TheModel,
+    TheFooter,
   },
 };
 </script>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="footer">
+    <div class="info">
+      <a href="https://www.facebook.com/DuyTh4i" target="_blank">ThaiDD</a
+      ><br />
+      <span>dev.thaidd@gmail.com</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#footer {
+  position: fixed;
+  bottom: 0;
+  width: 98%;
+  height: auto;
+}
+.info {
+  float: right;
+  text-align: center;
+  color: white;
+  font-size: 0.6em;
+}
+a {
+  color: white;
+  text-decoration: none;
+}
+a:hover {
+  color: rgb(233, 34, 74);
+}
+</style>
